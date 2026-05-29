08de13e00efc9a72f0b9f86539ff9e1c600232e1
ssgghshs <ssgghshs@163.com>
2026-05-30 00:51:44 +0800

    新增操作系统安装

--- install/install2.sh
+++ install/install2.sh
@@ -132,7 +132,7 @@ step_check_system() {
             DEV_PKGS="build-essential libssl-dev libbz2-dev libffi-dev zlib1g-dev libreadline-dev libsqlite3-dev tar ipset ufw"
             apt-get update -y
             ;;
-        centos|rhel|rocky|almalinux)
+        centos|rhel|rocky|almalinux|openEuler|kylin)
             PKG_MANAGER="dnf"
             PKG_INSTALL="dnf install -y"
             PYTHON_PKG="python3 python3-pip python3-devel"
